f42770dd0b3c13a754fbcd2b553340bb342adcac
Eugen Druzin <edruzin@gmail.com>
2018-12-07 11:08:37 +0100

    Initialize project

---
+++ etc/init/postgres/client-local.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export PGHOST=127.0.0.1
+export PGPORT=5432
+
+export PGUSER=postgres
+export PGPASSWORD=password
+
---
+++ initialize-local-database.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+source etc/init/postgres/client-local.sh
+
+psql -f etc/flyway/init/create_databases.sql
+
