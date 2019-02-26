0561cb059ad9f89775bd1dc3109668d0de251daa
Eugen Druzin <edruzin@gmail.com>
2019-02-26 15:25:55 +0100

    Add asciidoctor

--- src/test/kotlin/com/ampnet/crowdfunding/blockchain/controller/ControllerTestBase.kt
+++ src/test/kotlin/com/ampnet/crowdfunding/blockchain/controller/ControllerTestBase.kt
@@ -6,12 +6,17 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.restdocs.RestDocumentationContextProvider
+import org.springframework.restdocs.RestDocumentationExtension
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation
+import org.springframework.restdocs.operation.preprocess.Preprocessors
 import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
 
-@ExtendWith(value = [SpringExtension::class])
+@ExtendWith(value = [SpringExtension::class, RestDocumentationExtension::class])
 abstract class ControllerTestBase : TestBase() {
 
     @Autowired
@@ -22,7 +27,14 @@ abstract class ControllerTestBase : TestBase() {
     protected lateinit var mockMvc: MockMvc
 
     @BeforeEach
-    fun init(wac: WebApplicationContext) {
-        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build()
+    fun init(wac: WebApplicationContext, restDocumentation: RestDocumentationContextProvider) {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+                .apply<DefaultMockMvcBuilder>(MockMvcRestDocumentation.documentationConfiguration(restDocumentation))
+                .alwaysDo<DefaultMockMvcBuilder>(MockMvcRestDocumentation.document(
+                        "{ClassName}/{methodName}",
+                        Preprocessors.preprocessRequest(Preprocessors.prettyPrint()),
+                        Preprocessors.preprocessResponse(Preprocessors.prettyPrint())
+                ))
+                .build()
     }
 }
